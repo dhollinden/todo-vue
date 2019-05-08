7610b1ac80b50d1d4e33325dddadeb089f032724
dhollinden <dave.hollinden@ithaka.org>
2019-05-08 06:29:26 -0400

    Add links and logic to v-navigation-drawer

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,21 +1,42 @@
 <template>
-<!--
-      <div>
-        <router-link v-bind:to="{ name: 'Notes' }" class="">My Notes</router-link>&nbsp;&nbsp;
-        <router-link v-bind:to="{ name: 'MyAccount' }" class="">My Account</router-link>&nbsp;&nbsp;
-        <b-link @click="logout()">Logout</b-link>
-      </div>
--->
   <span>
         <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
             <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content>
-                            {{item.title}}
-                        </v-list-tile-content>
-                    </v-list-tile>
-                    <v-divider :key="`divider-${index}`"></v-divider>
+                <template>
+                  <div v-if="signedIn">
+                    <v-list-tile to="/Notes">
+                          <v-list-tile-content>
+                              NOTES
+                          </v-list-tile-content>
+                      </v-list-tile>
+                    <v-divider></v-divider>
+                    <v-list-tile v-on:click="logout">
+                          <v-list-tile-content>
+                              SIGN OUT
+                          </v-list-tile-content>
+                      </v-list-tile>
+                    <v-divider></v-divider>
+                    <v-list-tile to="/MyAccount">
+                          <v-list-tile-content>
+                              MY ACCOUNT
+                          </v-list-tile-content>
+                      </v-list-tile>
+                    <v-divider></v-divider>
+                  </div>
+                  <div v-else>
+                    <v-list-tile to="/Login">
+                          <v-list-tile-content>
+                              SIGN IN
+                          </v-list-tile-content>
+                      </v-list-tile>
+                    <v-divider></v-divider>
+                    <v-list-tile to="/Register">
+                          <v-list-tile-content>
+                              SIGN UP
+                          </v-list-tile-content>
+                      </v-list-tile>
+                    <v-divider></v-divider>
+                  </div>
                 </template>
             </v-list>
         </v-navigation-drawer>
@@ -23,12 +44,12 @@
             <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-btn flat class="hidden-sm-and-down" to="/Notes">Notes</v-btn>
+            <v-btn class="hidden-sm-and-down" flat to="/Notes" v-if="signedIn">Notes</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
             <v-btn class="hidden-sm-and-down" flat to="/Login" v-if="!signedIn">SIGN IN</v-btn>
             <v-btn class="hidden-sm-and-down" flat @click="logout" v-if="signedIn">SIGN OUT</v-btn>
             <v-btn class="hidden-sm-and-down" color="brown lighten-3" to="/Register" v-if="!signedIn">SIGN UP</v-btn>
-            <v-btn class="hidden-sm-and-down" color="brown lighten-3" to="/MyAccount" v-if="signedIn">MY ACCOUNT</v-btn>
+            <v-btn class="hidden-sm-and-down" flat to="/MyAccount" v-if="signedIn">MY ACCOUNT</v-btn>
         </v-toolbar>
     </span>
 </template>
@@ -45,9 +66,11 @@ export default {
       appTitle: 'Lister',
       drawer: false,
       items: [
-        { title: 'Notes' },
-        { title: 'Sign In' },
-        { title: 'Join' }
+        { title: 'Notes', icon: 'notes', dest: '/Notes', logic: this.signedIn },
+        { title: 'Sign In', dest: '/Login', logic: !this.signedIn },
+        { title: 'Sign Out', click: 'logout', logic: this.signedIn },
+        { title: 'Sign Up', dest: '/Register', logic: !this.signedIn },
+        { title: 'My Account', dest: '/MyAccount', logic: this.signedIn }
       ],
       signedIn: true
     }
@@ -56,6 +79,7 @@ export default {
     // start listening to eventBus as soon as NavBar component is created
     eventBus.$on('signedIn', (signedIn) => {
       this.signedIn = signedIn
+      console.log('NavBar.vue: eventBus.$on: signedIn = ', this.signedIn)
     })
   },
   methods: {
@@ -63,11 +87,12 @@ export default {
       const response = await AuthService.logout()
       if (response.data.success) {
         this.signedIn = false
+        this.drawer = false
         this.$router.push({ name: 'Login' })
       } else {
         this.errors = response.data.err
       }
-      console.log('NavBar: logout() - signedIn = ', this.signedIn)
+      console.log('NavBar: logout() function: signedIn = ', this.signedIn)
     }
   }
 }
