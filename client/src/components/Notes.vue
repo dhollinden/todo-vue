ed06c6a3c49f5b9c6cbecd4fe71d7769abb23436
dhollinden <dave.hollinden@ithaka.org>
2019-05-10 07:09:04 -0400

    Clean up Notes, get rid of old sections, etc.

--- client/src/components/Notes.vue
+++ client/src/components/Notes.vue
@@ -1,25 +1,8 @@
 <template>
-<!--
-  <b-row>
-    <b-col cols="12">
-      <ul v-if="errors && errors.length">
-        <li v-for="error of errors" :key="error.id">
-          <b-alert show>{{error.msg}}</b-alert>
-        </li>
-      </ul>
-      <b-table striped hover :items="notes" :fields="fields">
-        <template slot="actions" slot-scope="row">
-          <router-link v-bind:to="{ name: 'EditNote', params: { id: row.item._id } }">Edit</router-link> |
-          <a href="#" @click="confirmDeleteNote(row.item._id)">Delete</a>
-        </template>
-      </b-table>
-    </b-col>
-  </b-row>
--->
   <v-container grid-list-lg>
     <v-layout>
       <v-spacer></v-spacer>
-      <v-btn color="cyan darken-1" flat to="/AddNote" dark>ADD Note</v-btn>
+      <v-btn color="primary darken-1" flat to="/AddNote" dark>ADD Note</v-btn>
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12 sm12 md12>
@@ -40,23 +23,23 @@
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12 sm12 md6 v-for="(note, index) in notes" :key="index">
-          <v-toolbar color="cyan" dark>
+          <v-toolbar color="primary" dark>
             <v-toolbar-side-icon></v-toolbar-side-icon>
             <v-toolbar-title>{{ note.name }}</v-toolbar-title>
             <v-spacer></v-spacer>
-            <!-- for displaying only unchecked/checked items
+            <!-- for displaying only unchecked/checked items (for future use)
             <v-btn icon><v-icon>check_box_outline_blank</v-icon></v-btn>
             <v-btn icon><v-icon>check_box</v-icon></v-btn>
             -->
             <v-btn icon v-bind:to="{ name: 'EditNote', params: { id: note._id } }"><v-icon>edit</v-icon></v-btn>
-            <v-btn icon href="#" @click.stop="confirmDelete(note._id)"><v-icon>delete</v-icon></v-btn>
+            <v-btn icon href="#" @click.stop="confirmDelete(note._id, note.name)"><v-icon>delete</v-icon></v-btn>
           </v-toolbar>
 
           <v-list>
             <!-- the template could be <template v-for="(item, index) in items"> when there are multiple todos per list -->
             <template>
               <v-list-tile>
-                <!-- for initial checkbox
+                <!-- for initial checkbox (for future use)
                 <v-list-tile-action>
                   <span>
                     <v-btn icon><v-icon>check_box_outline_blank</v-icon></v-btn>
@@ -66,7 +49,7 @@
                 <v-list-tile-content>
                   <v-list-tile-title v-html="note.body"></v-list-tile-title>
                 </v-list-tile-content>
-                <!-- edit and delete buttons
+                <!-- edit and delete buttons (for future use)
                 <v-list-tile-action>
                   <span>
                     <v-btn icon><v-icon>edit</v-icon></v-btn>
@@ -83,11 +66,11 @@
       <v-dialog v-model="dialog" persistent max-width="290">
         <v-card>
           <v-card-title class="headline">Delete this note?</v-card-title>
-          <v-card-text>note ID to delete is  </v-card-text>
+          <v-card-text>Delete "{{ this.deleteName }}"? </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="green darken-1" flat @click="dialog = false">Cancel</v-btn>
-            <v-btn color="green darken-1" flat @click="deleteNote()">Delete</v-btn>
+            <v-btn color="primary darken-1" flat @click="dialog = false">Cancel</v-btn>
+            <v-btn color="primary darken-1" flat @click="deleteNote()">Delete</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -125,14 +108,16 @@ export default {
         this.alert = true
       }
     },
-    confirmDelete (id) {
+    confirmDelete (id, name) {
       this.dialog = true
       this.deleteId = id
+      this.deleteName = name
     },
     async deleteNote () {
       this.dialog = false
       const response = await TodoService.deleteNote(qs.stringify({ id: this.deleteId }))
       this.deleteId = ''
+      this.deleteName = ''
       if (response.data.success) {
         this.fetchNotes()
       } else {
