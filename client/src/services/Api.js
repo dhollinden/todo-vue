34792d7d9b51bfed97b80a759c03896fc3593261
Dave Hollinden <Dave.Hollinden@ithaka.org>
2019-06-04 07:34:38 -0400

    Add logic to configure baseURL depending on prod vs dev

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+const baseURL = process.env.NODE_ENV === 'production' ? `https://y5zkbh34gk.execute-api.us-east-1.amazonaws.com/dev` : `http://localhost:8080/api`
+console.log('Api.js: baseURL = ', baseURL)
+
 export default() => {
   return axios.create({
-    // baseURL: `http://localhost:8080/api`,
-    baseURL: `https://y5zkbh34gk.execute-api.us-east-1.amazonaws.com/dev`,
+    baseURL: baseURL,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
